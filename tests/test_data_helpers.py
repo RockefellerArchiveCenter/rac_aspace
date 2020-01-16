b6325d64475162f3fb3b06889a4210d8b681a3d4
Hillel Arnold <helrond@hotmail.com>
2020-01-16 17:46:15 -0500

    adds placeholder tests

---
+++ tests/test_data_helpers.py
@@ -0,0 +1,14 @@
+"""
+Unit tests for Data Helpers
+"""
+import unittest
+
+
+class TestDataHelpers(unittest.TestCase):
+
+    def test_helpers(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_serializers.py
@@ -0,0 +1,14 @@
+"""
+Unit tests for Serializers
+"""
+import unittest
+
+
+class TestSerializers(unittest.TestCase):
+
+    def test_serialization(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
