d09d4024e4748677571524fa883ddbc0ed61905a
Patrick <pgalligan@rockarch.org>
2020-02-24 11:25:46 -0500

    OOP approach
    First attempt at refactoring for OOP approach. Still trying to think through if we need more. This assumes the data will be in a dictionary format.

--- rac_aspace/serializers.py
+++ rac_aspace/serializers.py
@@ -1,60 +1,46 @@
 import argparse
-from configparser import ConfigParser
-# import csv
-import os
+import csv
 
+class Serializer:
 
-config = ConfigParser()
-config.read("local_settings.cfg")
+    def __init__(self, filename, delimiter, data):
+        self.filename = filename
+        self.fieldnames = list(data.keys())
+        self.data = data
+        self.delimiter = delimiter
 
-file_path = os.path.join(
-    os.path.abspath(os.path.dirname(__file__)), config.get(
-        "Destinations", "filename")
-)
+    def run(self):
+        with open(self.filename, 'w') as f:
+            writer = csv.DictWriter(f, fieldnames=fieldnames, delimiter=delimiter)
+            writer.writeheader()
+            writer.writerows(data)
 
-FILE_TYPE_CHOICES = ["csv", "tsv"]
+class CSVWriter(Serializer):
 
-column_headings = []
+    def delimiter(self):
+        return delimiter = "','"
 
-column = 'test'
-heading = 'data'
+class TSVWriter(Serializer):
 
-data = [column, heading]
-
-
-def open_file(args, file_path):
-    """"Opens file to be written to and sets serialization type"""
-    # file_type = args.file_type
-    # if file_type == 'csv':
-    # writer = csv.writer(open(file_path, 'w'))
-    # else:
-    # writer = csv.writer(open(file_path, delimiter='\t'))
-
-
-def create_headings(column_headings, writer):
-    """Creates a spreadsheet with column headings"""
-    writer.writerow(column_headings)
-
-
-def write_data(data, writer):
-    """"Writes data to a csv file"""
-    for d in data:
-        column_data = d.column
-        heading_data = d.heading
-        writer.writerow([column_data, heading_data])
-
+    def delimiter(self):
+        return delimiter = "'\t'"
 
 def get_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "file_type",
-        choices=FILE_TYPE_CHOICES,
-        help="The type of file you would like to output data to (tsv or csv)")
+    parser.add_argument("filename", help="The desired name of your output file.")
+    return parser
 
+def main():
+    data = {}
+    parser = get_parser()
+    args = parser.parse_args()
+    filename = args.filename
+    if 'csv' in filename:
+        write = CSVWriter(filename, data)
+        write.run()
+    else:
+        write = TSVWriter(filename, data)
+        write.run()
 
-parser = get_parser()
-args = parser.parse_args()
-global writer
-open_file(file_path)
-create_headings([column_headings])
-write_data(data)
+if __name__ == "__main__":
+    main()
