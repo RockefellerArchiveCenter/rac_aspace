f1374abe187ede41dcea1de6ff8481d4c0bcb360
Patrick <pgalligan@rockarch.org>
2020-02-24 11:40:52 -0500

    pass tests
    updates to pass test. A little unsure if this makes sense/works

--- rac_aspace/serializers.py
+++ rac_aspace/serializers.py
@@ -1,6 +1,7 @@
 import argparse
 import csv
 
+
 class Serializer:
 
     def __init__(self, filename, delimiter, data):
@@ -11,36 +12,50 @@ class Serializer:
 
     def run(self):
         with open(self.filename, 'w') as f:
-            writer = csv.DictWriter(f, fieldnames=fieldnames, delimiter=delimiter)
+            writer = csv.DictWriter(
+                f, fieldnames=fieldnames, delimiter=delimiter)
             writer.writeheader()
             writer.writerows(data)
+
 
 class CSVWriter(Serializer):
 
     def delimiter(self):
-        return delimiter = "','"
+        delimiter = "','"
+        return delimiter
+
 
 class TSVWriter(Serializer):
 
     def delimiter(self):
-        return delimiter = "'\t'"
+        delimiter = "'\t'"
+        return delimiter
+
 
 def get_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("filename", help="The desired name of your output file.")
+    parser.add_argument(
+        "filename",
+        help="The desired name of your output file.")
     return parser
 
+
+data = {}
+fieldnames = []
+delimiter = ''
+parser = get_parser()
+args = parser.parse_args()
+filename = args.filename
+
+
 def main():
-    data = {}
-    parser = get_parser()
-    args = parser.parse_args()
-    filename = args.filename
     if '.csv' in filename:
         write = CSVWriter(filename, data)
         write.run()
     else:
         write = TSVWriter(filename, data)
         write.run()
+
 
 if __name__ == "__main__":
     main()
