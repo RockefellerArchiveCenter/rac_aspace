657c4e9970e8b66c26c5fea09879e17a499b1d39
Patrick <pgalligan@rockarch.org>
2020-02-27 10:05:55 -0500

    change function being tested
    Change function being tested

--- tests/test_data_helpers.py
+++ tests/test_data_helpers.py
@@ -2,12 +2,16 @@
 Unit tests for Data Helpers
 """
 import unittest
+from .data_helpers import get_locations
 
 
 class TestDataHelpers(unittest.TestCase):
+    """
+    Tests the data helper functions.
+    """
 
-    def test_helpers(self):
-        pass
+    def test_get_locations(self):
+        self.assertTrue(len(locations))
 
 
 if __name__ == '__main__':
