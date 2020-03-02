1b0dd645e110884cf4fb6032470a8f22b1fd1852
p-galligan <pjg250@gmail.com>
2020-03-02 15:00:11 -0500

    Update rac_aspace/decorators.py
    Co-Authored-By: Hillel Arnold <helrond@hotmail.com>

--- rac_aspace/decorators.py
+++ rac_aspace/decorators.py
@@ -3,7 +3,6 @@
 def check_dictionary(func):
     def inner(*args, **kwargs):
         if not isinstance(args, dict):
-            print('Parameter is not a dictionary')
-            return
+            raise TypeException('{} is not a dictionary'.format(args[0])
         return func(*args, **kwargs)
     return inner
