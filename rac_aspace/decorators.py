56f3eb2bde642d62fd3b121754df05dca706514c
Hillel Arnold <helrond@hotmail.com>
2020-03-03 16:41:26 -0500

    target correct exception

--- rac_aspace/decorators.py
+++ rac_aspace/decorators.py
@@ -3,6 +3,6 @@
 def check_dictionary(func):
     def inner(*args, **kwargs):
         if not [isinstance(a, dict) for a in args]:
-            raise TypeException('{} is not a dictionary'.format(args[0]))
+            raise TypeError('{} is not a dictionary'.format(args[0]))
         return func(*args, **kwargs)
     return inner
