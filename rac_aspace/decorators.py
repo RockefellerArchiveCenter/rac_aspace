6bc3154cd99714a12a4cad726a78d9430b52965c
Patrick <pgalligan@rockarch.org>
2020-03-09 15:55:09 -0400

    adding to decorators
    Adding some functionality to decorators.

--- rac_aspace/decorators.py
+++ rac_aspace/decorators.py
@@ -4,5 +4,35 @@ def check_dictionary(func):
     def inner(*args, **kwargs):
         if not [isinstance(a, dict) for a in args]:
             raise TypeError('{} is not a dictionary'.format(args[0]))
+        if not [a for a in args]:
+            raise AttributeError('{} is empty'.format(args[0]))
+        return func(*args, **kwargs)
+    return inner
+
+
+def check_list(func):
+    def inner(*args, **kwargs):
+        if not [isinstance(a, list) for a in args]:
+            raise TypeError('{} is not a list'.format(args[0]))
+        if [len(a)==0 for a in args]:
+            raise AttributeError('{} is empty'.format(args[0]))
+        return func(*args, **kwargs)
+    return inner
+
+
+def validate_dictionary(func):
+    def inner(*args, **kwargs):
+        data = func(*args, **kwargs)
+        if not isinstance(data, dict):
+            raise TypeError('{} is not a dictionary'.format(args[0]))
+        return func(*args, **kwargs)
+    return inner
+
+
+def validate_list(func):
+    def inner(*args, **kwargs):
+        data = func(*args, **kwargs)
+        if not isinstance(data, list):
+            raise TypeError('{} is not a dictionary'.format(args[0]))
         return func(*args, **kwargs)
     return inner
