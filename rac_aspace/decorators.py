942a959b02c8369ab17a82b636182bc36697a6fd
daremiyo <dyoung@rockarch.org>
2020-04-07 15:10:01 -0400

    add wraps decorator
    fixes #86

--- rac_aspace/decorators.py
+++ rac_aspace/decorators.py
@@ -1,5 +1,7 @@
+from functools import wraps
 def check_type(obj_type):
     def real_decorator(func):
+        @wraps(func)
         def wrapper(*args, **kwargs):
             if not isinstance(args[0], obj_type):
                 raise TypeError("{} is not a {}".format(args[0], obj_type))
